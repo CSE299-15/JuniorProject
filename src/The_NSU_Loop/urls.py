3961a9d1f452e91edf4715e9f61a3fb5efedf711
simonakter <simonbinakter8@gmail.com>
2021-05-18 16:43:12 +0600

    Setup Simon branch for merging

--- src/The_NSU_Loop/urls.py
+++ src/The_NSU_Loop/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls.static import static
 from .views import home_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home_view, name='home-view'),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('jackpots/', include('jackpot.urls', namespace='jackpots')),
 ]
